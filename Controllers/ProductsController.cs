304d1c2a31b3fd4eab04879511778e35cd9cb9c8
Hamza-777 <hamzararrani77@gmail.com>
2022-09-26 23:14:06 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PracticeAPI.Models;
+
+namespace PracticeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly apipracticeContext db;
+
+        public ProductsController(apipracticeContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IQueryable<Product>>> getProducts()
+        {
+            return Ok(await db.Products.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> getProductById(int id)
+        {
+            return Ok(await db.Products.FindAsync(id));
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult<IQueryable<Product>>> getProductByPriceAndManufacturer(float price, string manufacturer)
+        //{
+        //    return Ok(await db.Products.Where(x => x.Price == price && x.Manufacturedby == manufacturer).SingleOrDefaultAsync());
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult> AddProduct([FromBody] Product prd)
+        {
+            if(prd == null)
+            {
+                return BadRequest();
+            } 
+            
+            if(ModelState.IsValid)
+            {
+                db.Products.Add(prd);
+                await db.SaveChangesAsync();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult> UpdateProduct(int id, [FromBody] Product prd)
+        {
+            if (prd == null)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Update(prd);
+                await db.SaveChangesAsync();
+            }
+
+            return Ok(prd);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            Product prd = db.Products.Find(id);
+            db.Products.Remove(prd);
+            await db.SaveChangesAsync();
+            return Ok(prd);
+        }
+    }
+}
---
+++ Models/apipracticeContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PracticeAPI.Models
+{
+    public partial class apipracticeContext : DbContext
+    {
+        public apipracticeContext()
+        {
+        }
+
+        public apipracticeContext(DbContextOptions<apipracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=apipractice;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("products");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Expirydate)
+                    .HasColumnType("date")
+                    .HasColumnName("expirydate");
+
+                entity.Property(e => e.Manufacturedate)
+                    .HasColumnType("date")
+                    .HasColumnName("manufacturedate");
+
+                entity.Property(e => e.Manufacturedby)
+                    .IsUnicode(false)
+                    .HasColumnName("manufacturedby");
+
+                entity.Property(e => e.Name)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Price).HasColumnName("price");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
